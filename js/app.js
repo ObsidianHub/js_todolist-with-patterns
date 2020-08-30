ff279961547bcfaea7733f6a8188645ef89d21c0
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-08-31 01:02:20 +0300

    init elements in app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@ const tasks = Tasks.getInstance();
 const addTaskObserver = new EventObserver();
 const removeTaskObserver = new EventObserver();
 const removeAllTasksObserver = new EventObserver();
+
+// Init elements
+const form = document.forms["addTodoItem"];
+const inputText = form.elements["todoText"];
+const ul = document.querySelector(".list-group");
+const clearBtn = document.querySelector(".clear-btn");
