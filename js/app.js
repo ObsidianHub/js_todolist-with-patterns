862d2d340e70979c10a8c232e2a3dbe91b94ff87
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-08-31 23:52:20 +0300

    create all subscribers in app.js

--- js/app.js
+++ js/app.js
@@ -7,6 +7,19 @@ const localstorage = Localstorage;
 // Init Notification module
 const notification = Notification;
 
+// Subscribe on add task event
+addTaskObserver.subscribe(localstorage.update);
+addTaskObserver.subscribe(notification.show);
+addTaskObserver.subscribe(ui.checkList);
+
+removeTaskObserver.subscribe(localstorage.update);
+removeTaskObserver.subscribe(notification.show);
+removeTaskObserver.subscribe(ui.checkList);
+
+removeAllTasksObserver.subscribe(localstorage.update);
+removeAllTasksObserver.subscribe(notification.show);
+removeAllTasksObserver.subscribe(ui.checkList);
+
 // Init Ovservers
 const addTaskObserver = new EventObserver();
 const removeTaskObserver = new EventObserver();
